5d88bd6b3e5c25ff26442818d3268fb5984a51aa
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-03-20 04:17:26 -0300

    update

--- src/app/pages/treasury/outflow/outflow-report-page/outflow-report-page.component.ts
+++ src/app/pages/treasury/outflow/outflow-report-page/outflow-report-page.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OutFlowHandler } from 'src/app/handlers/outflowHandler';
 
 @Component({
   selector: 'app-outflow-report-page',
   templateUrl: './outflow-report-page.component.html'
 })
-export class OutflowReportPageComponent {
+export class OutflowReportPageComponent implements OnInit {
+  protected busy = false;
 
+  constructor(private handler: OutFlowHandler) {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
