b00f5e62d5a32eec5ba71e36429528ce775e6f33
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-05 11:41:36 -0300

    feat: initial report secretary

--- src/app/services/user.services.ts
+++ src/app/services/user.services.ts
@@ -1,14 +1,35 @@
 import { Injectable } from "@angular/core";
 import { BaseService } from "./base.services";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AuthService } from "./auth.services";
+import { ResultViewModel } from "../models/churchEntitieModels/resultViewModel.models";
 
 @Injectable({
     providedIn: 'root'
 })
 
-export  class UserServices extends BaseService {
+export class UserServices extends BaseService {
     constructor(http: HttpClient) {
         super(http);
         this.modelName = "user";
+    }
+
+    public async getAll() {
+        var auth = new AuthService();
+        const token = auth.getToken();
+
+        const httpHeaders = new HttpHeaders()
+            .set("Content-Type", "application/json; charset=utf-8")
+            .set("Authorization", `Bearer ${JSON.parse(token)}`);
+
+        const returnObservable = this.http.get<ResultViewModel>(`${this.url}/v1/${this.modelName}/`, { headers: httpHeaders }).toPromise();
+
+        return returnObservable.then(result => {
+            if (result) {
+                return result;
+            } else {
+                throw new Error('Result is undefined.');
+            }
+        });
     }
 }
