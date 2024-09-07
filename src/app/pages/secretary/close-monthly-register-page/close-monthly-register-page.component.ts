92359b52325b2f834edaefcff226deab203424c1
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-09-07 12:00:25 -0300

    create page for close monthly

---
+++ src/app/pages/secretary/close-monthly-register-page/close-monthly-register-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-close-monthly-register-page',
+  templateUrl: './close-monthly-register-page.component.html'
+})
+export class CloseMonthlyRegisterPageComponent {
+
+}
