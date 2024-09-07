475bd9f6b26967f6c508d79e6461421523ef9c14
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-09-07 12:47:07 -0300

    search churchs on dashboard

--- src/app/pages/secretary/close-monthly-register-page/close-monthly-register-page.component.ts
+++ src/app/pages/secretary/close-monthly-register-page/close-monthly-register-page.component.ts
@@ -1,4 +1,9 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChurchHadler } from 'src/app/handlers/churchHandler';
+import { ResultViewModel } from 'src/app/models/churchEntitieModels/resultViewModel.models';
+import { ChurchReadModel } from 'src/app/models/ReadModels/ChurchRead.models';
 
 @Component({
   selector: 'app-close-monthly-register-page',
@@ -6,4 +11,68 @@ import { Component } from '@angular/core';
 })
 export class CloseMonthlyRegisterPageComponent {
 
+  protected msgSuccesss: string[] = [];
+  protected msgErros: string[] = [];
+  protected busy: boolean = false;
+  
+  protected churchToSelect!: [string, string][]
+  protected churchs!: ResultViewModel['data'];
+  
+  protected formSearchChurch!: FormGroup;
+
+  constructor(private fbuilder: FormBuilder, private churchHandler: ChurchHadler) {
+    this.formSearchChurch = this.fbuilder.group({
+      churchId: ['', Validators.compose([
+        Validators.required
+      ])],
+    });
+  }
+
+  ngOnInit(): void {
+    this.dashBoard();
+  }
+
+  protected async dashBoard() {
+    this.msgErros = [];
+    this.msgSuccesss = [];
+    this.formSearchChurch.reset();
+    
+    console.log('dashBoard');
+
+    await this.loadChurchs();
+  }
+
+  private async loadChurchs() {
+    try {
+      const dados = await this.churchHandler.getChurchByPeriod(
+        formatDate(new Date(1900, 1, 1), 'yyyy-MM-dd', 'en'),
+        formatDate(new Date(), 'yyyy-MM-dd', 'en')
+      );
+      this.churchs = dados.data;
+
+      var meuObjeto: Record<string, string> = {};
+      this.churchs.forEach((x: ChurchReadModel) => {
+        var key = x.name;
+        var value = x.id;
+
+        meuObjeto[key] = `${value}`;
+      });
+
+      this.churchToSelect = Object.entries(meuObjeto);
+    } catch (error) {
+      console.log('error to get churchs:', error);
+    }
+  }
+
+  protected loadReport() {
+    this.msgErros = [];
+
+    var church = this.formSearchChurch.controls['churchId'].value;
+    if (church === '') {
+      this.msgErros.push('Selecione uma igreja');
+      return;
+    }
+
+        
+  }
 }
