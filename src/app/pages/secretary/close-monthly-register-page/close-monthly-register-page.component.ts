f05c4df029eeb4c4a55fcbe802a9f1dfd06bdd55
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-09-08 17:02:25 -0300

    updating page close monthly

--- src/app/pages/secretary/close-monthly-register-page/close-monthly-register-page.component.ts
+++ src/app/pages/secretary/close-monthly-register-page/close-monthly-register-page.component.ts
@@ -2,6 +2,7 @@ import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChurchHadler } from 'src/app/handlers/churchHandler';
+import { CloseMonthlyHandler } from 'src/app/handlers/closeMonthlyHandler';
 import { ResultViewModel } from 'src/app/models/churchEntitieModels/resultViewModel.models';
 import { ChurchReadModel } from 'src/app/models/ReadModels/ChurchRead.models';
 
@@ -13,16 +14,21 @@ export class CloseMonthlyRegisterPageComponent {
 
   protected msgSuccesss: string[] = [];
   protected msgErros: string[] = [];
+  protected yearsToSearch: number[] = [];
+  protected currentYear: number = new Date().getFullYear(); //PEGAR ANO CARREGADO NO DASHBOARD
   protected busy: boolean = false;
-  
+
   protected churchToSelect!: [string, string][]
   protected churchs!: ResultViewModel['data'];
-  
+
   protected formSearchChurch!: FormGroup;
 
-  constructor(private fbuilder: FormBuilder, private churchHandler: ChurchHadler) {
+  constructor(private fbuilder: FormBuilder, private churchHandler: ChurchHadler, private handler: CloseMonthlyHandler) {
     this.formSearchChurch = this.fbuilder.group({
       churchId: ['', Validators.compose([
+        Validators.required
+      ])],
+      year: ['', Validators.compose([
         Validators.required
       ])],
     });
@@ -36,10 +42,20 @@ export class CloseMonthlyRegisterPageComponent {
     this.msgErros = [];
     this.msgSuccesss = [];
     this.formSearchChurch.reset();
-    
+
     console.log('dashBoard');
 
     await this.loadChurchs();
+    this.loadYears();
+
+    this.loadReport();
+  }
+
+  private async loadYears() {
+    this.yearsToSearch.push(new Date().getFullYear() - 2);
+    this.yearsToSearch.push(new Date().getFullYear() - 1);
+    this.yearsToSearch.push(new Date().getFullYear());
+    this.yearsToSearch.push(new Date().getFullYear() + 1);
   }
 
   private async loadChurchs() {
@@ -68,11 +84,13 @@ export class CloseMonthlyRegisterPageComponent {
     this.msgErros = [];
 
     var church = this.formSearchChurch.controls['churchId'].value;
-    if (church === '') {
-      this.msgErros.push('Selecione uma igreja');
-      return;
-    }
+    var year = this.formSearchChurch.controls['year'].value;
+    console.log(year);
+    console.log(church);
 
-        
+    this.msgErros.push('Selecione uma igreja');
+
+    var resultHandler = this.handler.getAllByYear(year);
+    console.log(resultHandler);
   }
 }
