6eb4bfd03f76ee482a05472995c40cd75752e258
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-09-09 21:58:11 -0300

    create model for closemonthly

---
+++ src/app/models/churchEntitieModels/CloseMonthly.models.ts
@@ -0,0 +1,8 @@
+import { EntitieResultApi } from "./Entitie.models";
+
+export class CloseMonthly extends EntitieResultApi {
+    public yeahMonth: string = "";
+    public block: string = "";
+    public church: string = "";
+}
+  
